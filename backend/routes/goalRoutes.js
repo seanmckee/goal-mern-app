b2d095918fea940a8c06c73aee1ca0ddc44f9798
seanmckee <seanmingmckee@gmail.com>
2023-02-10 05:01:00 -0800

    api finished

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
 router.route('/').get(getGoals).post(setGoal)
-router.route('./:id').delete(deleteGoal).put(updateGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 
 module.exports = router
