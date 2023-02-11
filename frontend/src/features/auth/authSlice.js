72647afaffb454a369f363ec734a62bfa91e5319
seanmckee <seanmingmckee@gmail.com>
2023-02-10 16:44:57 -0800

    save progress

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+// Get user from localStorage
+
+const user = JSON.parse(localStorage.getItem('user'))
+
+const initialState = {
+    user: user ? user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reduces: {
+        reset: (state) =>{
+            state.isLoading = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
+        }
+    },
+    extraReducers: () => {}
+
+})
+
+
+export const {reset} = authSlice.actions
+export default authSlice.reducer
